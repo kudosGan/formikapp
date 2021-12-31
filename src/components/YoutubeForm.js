5d41cafa7d9495cea37ba26bb5f45405de8d5366
kudas ganesan <kudosdesigner@hotmail.com>
2021-12-31 14:15:33 -0500

    useFormick  handleChange

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,18 +1,30 @@
 import React from 'react'
+import{useFormik} from 'formik'
 
-function youtubeForm() {
+function YoutubeForm() {
+
+    const formik =useFormik({
+        initialValues: {
+            name:'',
+            email:'',
+            channel:'',
+        }
+    })
+
+    console.log ('Form values', formik.values)
+
     return (
         <div>
             <form>
 
                 <lable htmlFor='name'> Name  </lable>
-                <input type='text' id='name' name='name'/>
+                <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
 
                 <lable htmlFor='email'> E-mail </lable>
-                <input type='email' id='email' name='email'/>
+                <input type='email' id='email' name='email'onChange={formik.handleChange} value={formik.values.email}/>
 
                 <lable htmlFor='channel'> Channel </lable>
-                <input type='text' id='channel' name='channel'/>
+                <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
 
                 <button>Submit</button>
             </form>
@@ -21,4 +33,4 @@ function youtubeForm() {
     )
 }
 
-export default youtubeForm
+export default YoutubeForm
